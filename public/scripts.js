49e5f363023be9bff383f661c31e9f929eb28416
JefryTP <jefrytuncar@gmail.com>
2023-08-27 22:29:49 -0500

    Nuevo avance

--- public/scripts.js
+++ public/scripts.js
@@ -46,7 +46,7 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 window.watsonAssistantChatOptions = {
-    integrationID: "b28a8ac8-a4cc-4f36-8309-4a48636efc0e", // The ID of this integration.
+    integrationID: "b28a8ac8-a4cc-4f36-8309-4a48636efc0e", //Hola The ID of this integration.
     region: "au-syd", // The region your integration is hosted in.
     serviceInstanceID: "bd4b2ffc-a859-4bff-9f57-ac551da4a3bd", // The ID of your service instance.
     onLoad: function(instance) { instance.render(); }
@@ -56,3 +56,4 @@ window.watsonAssistantChatOptions = {
     t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
     document.head.appendChild(t);
   });
+  
